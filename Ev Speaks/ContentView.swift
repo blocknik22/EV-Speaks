fd342512451a4734b6924c5cfbc6237dae7e8c75
Nik V <nikhilvar85@gmail.com>
2025-05-27 23:06:48 -0400

    Initial Commit

---
+++ Ev Speaks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ev Speaks
+//
+//  Created by Nik on 5/27/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ev Speaks/Ev_SpeaksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ev_SpeaksApp.swift
+//  Ev Speaks
+//
+//  Created by Nik on 5/27/25.
+//
+
+import SwiftUI
+
+@main
+struct Ev_SpeaksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
